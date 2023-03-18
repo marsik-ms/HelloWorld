65bee90122b52bb12449d4ce76b90bd618a5930d
marsik-ms <79077490+marsik-ms@users.noreply.github.com>
2023-03-18 17:35:19 +0200

    work with view controller

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greentingLabel: UILabel!
+    @IBOutlet var greentingButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        greentingLabel.isHidden.toggle()
+        greentingButton.layer.cornerRadius = 10
+    }
     
+    @IBAction func greentingButtonDidTapped() {
+        greentingLabel.isHidden.toggle()
+        greentingButton.setTitle(
+        greentingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+        for: .normal
+        )
     }
 }
 
